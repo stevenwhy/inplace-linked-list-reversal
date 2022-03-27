38615aa4ef125a26917eeb3e0628d6d29c1351e8
stevenwhy <stevenyildirim@gmail.com>
2022-03-27 14:28:15 -0400

    reverse k-element subsets

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,12 +16,53 @@ fun main() {
     head.next?.next = Node(3)
     head.next?.next?.next = Node(4)
     head.next?.next?.next?.next = Node(5)
-    println("#1 Reverse a subset linked list")
+    println("#2 Reverse a subset linked list")
     head.print()
-    val reverseSubset = reverseSubset(head, 1, 4)
+    val reverseSubset = reverseSubset(head, 2, 4)
     reverseSubset.print()
-}
 
+    head.next?.next?.next?.next?.next = Node(6)
+    head.next?.next?.next?.next?.next?.next = Node(7)
+    head.next?.next?.next?.next?.next?.next?.next = Node(8)
+    println("#3 Reverse every k-element subset linked list")
+    head.print()
+    val reverseKSubList = reverseKSubset(head, 3)
+    reverseKSubList?.print()
+}
+/*
+    Given the head of a LinkedList and a number ‘k’,
+    reverse every ‘k’ sized sub-list starting from the head.
+ */
+fun reverseKSubset(head: Node, k: Int): Node? {
+    if(head.next == null) return head
+
+    var current: Node? = head
+    var previous: Node? = null
+    var resultHead: Node? = head
+    while(true) {
+        val start = previous
+        val last = current
+        var count = 0
+        while(current != null && count < k) {
+            val temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+            count++
+        }
+
+        // connect to previous part
+        if(start != null) {
+            start.next = previous
+        } else resultHead = previous
+
+        last?.next = current
+
+        if(current == null) break
+        previous = last
+    }
+    return resultHead
+}
 /*
     Given the head of a LinkedList and two positions ‘p’ and ‘q’,
     reverse the LinkedList from position ‘p’ to ‘q’.
