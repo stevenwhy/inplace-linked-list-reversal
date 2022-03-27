c24a3e2efe673d79553a6f07b71294eb9667689f
stevenwhy <stevenyildirim@gmail.com>
2022-03-27 10:44:20 -0400

    reverse subset

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,52 @@ fun main() {
     head.print()
     val reverse = reverse(head)
     reverse?.print()
+
+    head = Node(1)
+    head.next = Node(2)
+    head.next?.next = Node(3)
+    head.next?.next?.next = Node(4)
+    head.next?.next?.next?.next = Node(5)
+    println("#1 Reverse a subset linked list")
+    head.print()
+    val reverseSubset = reverseSubset(head, 1, 4)
+    reverseSubset.print()
+}
+
+/*
+    Given the head of a LinkedList and two positions ‘p’ and ‘q’,
+    reverse the LinkedList from position ‘p’ to ‘q’.
+ */
+fun reverseSubset(head: Node, p: Int, q: Int): Node {
+    // traverse p-1 times
+    var result = head
+    var count = 1
+    var current: Node? = head
+    var previous: Node? = null
+    while(count < p) {
+            if(current?.next != null) {
+                previous = current
+                current = current.next!!
+            } else return head // p is out of bounds
+            count++
+    }
+    val start = previous
+    val pNode = current
+    previous = current
+    if(current?.next != null) current = current.next!!
+    else return head // p is last element
+
+    while(count < q && current != null) {
+        val temp = current.next
+        current.next = previous
+        pNode?.next = temp
+        previous = current
+        current = temp
+        count++
+    }
+    if(start!= null) start.next = previous
+    else result = previous ?: head
+    return result
 }
 
 fun reverse(head: Node): Node? {
